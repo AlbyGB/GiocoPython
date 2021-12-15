7420c03b4f83e405094e165b87b2700861239d24
Alberto <alby0705@gmail.com>
2021-12-15 22:38:09 +0100

    Visibilità Mouse

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from textwrap import fill
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+import keyboard
 
 app = Ursina()
 window.borderless = False
+mouse.visible = False
 
 player = Entity(model='cube', color=color.red, scale=1, collider='box', position=(5,0,0))
 #player = FirstPersonController() to make the game in the first person
@@ -16,12 +17,16 @@ def get_input(key):
     player.x -= held_keys['a'] * 0.05
     player.y += held_keys['w'] * 0.05
     player.y -= held_keys['s'] * 0.05
-    if keys == 'escape':
-        quit()
 
-#commento di prova
+    if keyboard.is_pressed('escape') and mouse.visible == False:
+        mouse.visible = True
+    elif keyboard.is_pressed('escape') and mouse.visible == True:
+        mouse.visible = False
+
+
 def update():
     get_input(keys)
+
 
 box = Entity(model='cube', color=color.magenta, scale=(1, 2, 1)  ,collider='mesh')
 
